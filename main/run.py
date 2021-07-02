7eebce96e24dd16885243caefac83d80e5cef074
Matheus Rodrigues <mr634580@gmail.com>
2021-07-02 16:06:27 -0300

    altera path p/ relativo

--- main/run.py
+++ main/run.py
@@ -6,24 +6,24 @@
 from atividade import *
 from pathlib import Path
 
-path = Path("C:/Users\matheus.DESKTOP-MTRS8/pweb-env/bd_env/association-rules-data-mining/files/")
+path = Path("files/")
 
 file = path / "tabela-de-transacao.xlsx"
 
 data = fetch_data(file)
-produtos = getProducts(data)
-itens= len(produtos)
+products = getProducts(data)
+itens= len(products)
 
 for i in range(0, itens):
-    x = get_values_cells(produtos[i], data)
+    x = get_values_cells(products[i], data)
     for j in range(0,itens):
         if i != j:
-            y = get_values_cells(produtos[j], data)
+            y = get_values_cells(products[j], data)
             suporte= float(compareTo(x, y) / 10)
             confianca= float(compareTo(x, y) / len(x))
-            print("\n ", produtos[i], " e ", produtos[j])
+            print("\n", products[i], " e ", products[j])
             print("Suporte: ", suporte)
-            print(" >Confiança:", confianca)
+            print("Confiança:", confianca)
             print("\n")
     
 x_values = get_values_cells("leite", data)
@@ -32,14 +32,14 @@ suporte = float(compareTo(x_values, y_values)/10)
 confianca= float(compareTo(x_values, y_values)/len(x_values))
 
 print("\nLeite e café:")
-print(" >Suporte:", suporte)
-print(" >Confiança:", confianca)
+print("Suporte:", suporte)
+print("Confiança:", confianca)
 
 x_values= get_values_cells("pão", data)
 y_values= get_values_cells("manteiga", data)
-suporte = float(compareTo(x_values,y_values)/10)
-confianca = float(compareTo(x_values,y_values)/len(x_values))
+suporte = float(compareTo(x_values, y_values)/10)
+confianca = float(compareTo(x_values, y_values)/len(x_values))
 
 print("\nPão e manteiga:")
-print(" >Suporte:", suporte)
-print(" >Confiança:", confianca)
+print("Suporte:", suporte)
+print("Confiança:", confianca)
